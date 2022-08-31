8c5d243a46b431b6297bb6a67227b319927bcc6c
Jose Obreque Fuentes <j.obreque.fuentes@gmail.com>
2022-08-30 21:56:14 -0400

    Commit inicial

---
+++ src/main/java/com/example/ejerciciobackendbsale/EjercicioBackendBsaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciobackendbsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioBackendBsaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioBackendBsaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejerciciobackendbsale/EjercicioBackendBsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciobackendbsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioBackendBsaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
